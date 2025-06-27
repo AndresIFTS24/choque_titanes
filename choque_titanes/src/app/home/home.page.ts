1dd42e2e46e60db705af4726061e7dd51c2145f4
CarolinaT <27895429@ifts24.edu.ar>
2025-06-26 22:36:59 -0300

    boton logout

--- choque_titanes/src/app/home/home.page.ts
+++ choque_titanes/src/app/home/home.page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {IonicModule} from '@ionic/angular'
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonicModule],
 })
 export class HomePage {
-  constructor() {}
+  constructor(private auth: AuthService) {}
+
+  cerrarSesion(){
+    this.auth.cerrarSesion();
+  
+  }
 }
