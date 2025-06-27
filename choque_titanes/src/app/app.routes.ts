4f7a5210087fb0ddf1040afb478acb42817a8644
CarolinaT <27895429@ifts24.edu.ar>
2025-06-26 22:16:56 -0300

    authguard y login page

--- choque_titanes/src/app/app.routes.ts
+++ choque_titanes/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
   {
     path: 'mapa',
-    loadComponent: () => import('./mapa/mapa.page').then((m) => m.MapaPage),
+    loadComponent: () => import('./mapa/mapa.page').then((m) => m.MapaPage), canActivate: [authGuard]
   },
   {
     path: '',
@@ -13,5 +14,9 @@ export const routes: Routes = [
   {
     path: 'mapa',
     loadComponent: () => import('./mapa/mapa.page').then( m => m.MapaPage)
+  },  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
   },
+
 ];
