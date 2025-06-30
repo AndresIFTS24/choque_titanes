a0043078c521c22e40912b0fd4b6cf895ef53f1e
AugustoMarin <augustomarinangeli91@gmail.com>
2025-06-30 00:40:09 -0300

    Logica para correr la aplicacion en segundo plano

--- choque_titanes/src/app/app.component.ts
+++ choque_titanes/src/app/app.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { Platform } from '@ionic/angular';
+import { BackgroundMode } from '@awesome-cordova-plugins/background-mode/ngx';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   imports: [IonApp, IonRouterOutlet],
+  providers: [BackgroundMode]
 })
+
 export class AppComponent {
-  constructor() {}
+  constructor(private platform: Platform, private backgroundMode: BackgroundMode) {
+    this.initializeApp();
+  }
+
+  async initializeApp() {
+    await this.platform.ready();
+
+    // Activar modo segundo plano
+    this.backgroundMode.enable();
+    this.backgroundMode.on('activate').subscribe(() => {
+      console.log('Modo segundo plano activado');
+    });
+
+  }
+
 }
